cf5bea2e960e1f020b4d47034a0f25cdcf04f9c1
computbiol <48988788+computbiol@users.noreply.github.com>
2022-11-01 10:57:56 +0800

    fix: support for url checking other than host

--- app/checker.py
+++ app/checker.py
@@ -1,9 +1,33 @@
 import asyncio
-from http.client import HTTPConnection
-from urllib.parse import urlparse
 import aiohttp
+import logging
+from typing import Tuple
+from socket import gaierror
+from aiohttp.client_exceptions import TooManyRedirects
 import socket
 from contextlib import closing
+
+
+HEADERS = {
+    'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
+                   'AppleWebKit/537.36 (KHTML, like Gecko) '
+                   'Chrome/45.0.2454.101 Safari/537.36'),
+}
+
+
+class HttpClient:
+    session: aiohttp.ClientSession = None
+
+    def start(self):
+        self.session = aiohttp.ClientSession()
+
+    async def stop(self):
+        await self.session.close()
+        self.session = None
+
+    def __call__(self) -> aiohttp.ClientSession:
+        assert self.session is not None
+        return self.session
 
 
 def check_socket(host, port):
@@ -16,44 +40,57 @@ def check_socket(host, port):
             return False
 
 
-# Source: Leodanis Pozo Ramos - Email: leodanis@realpython.com
-def site_is_online(url, timeout=2):
-    """Return True if the target URL is online.
+def add_scheme(url: str):
+    urls = []
+    if url.startswith("https://"):
+        https_url = url
+        http_url = url.replace('https://', 'http://')
+        urls.append(https_url)
+        urls.append(http_url)
+    elif url.startswith("http://"):
+        https_url = url.replace('http://', 'https://')
+        http_url = url
+        urls.append(https_url)
+        urls.append(http_url)
+    else:
+        https_url = f"https://{url}"
+        http_url = f"http://{url}"
+        urls.append(https_url)
+        urls.append(http_url)
+    return urls
 
-    Raise an exception otherwise.
-    """
-    error = Exception("unknown error")
-    parser = urlparse(url)
-    host = parser.netloc or parser.path.split("/")[0]
-    for port in (80, 443):
-        connection = HTTPConnection(host=host, port=port, timeout=timeout)
-        try:
-            connection.request("HEAD", "/")
-            return True
-        except Exception as e:
-            error = e
-        finally:
-            connection.close()
-    raise error
 
-
-# Source: Modified from Leodanis Pozo Ramos - Email: leodanis@realpython.com
-async def site_is_online_async(url, proxy=None, timeout=2):
-    """Return True if the target URL is online.
-
-    Raise an exception otherwise.
-    """
-    error = Exception("unknown error")
-    parser = urlparse(url)
-    host = parser.netloc or parser.path.split("/")[0]
-    for scheme in ("http", "https"):
-        target_url = scheme + "://" + host
-        async with aiohttp.ClientSession() as session:
-            try:
-                await session.head(target_url, allow_redirects=True, timeout=timeout, proxy=proxy)
-                return True
-            except asyncio.TimeoutError:
-                error = Exception("timed out")
-            except Exception as e:
-                error = e
-    raise error
+async def get_status_code(session: aiohttp.ClientSession, url: str, proxy: str) -> Tuple[int, str]:
+    try:
+        # A HEAD request is quicker than a GET request
+        resp = await session.head(url, allow_redirects=True, proxy=proxy, ssl=False, headers=HEADERS)
+        async with resp:
+            status = resp.status
+            reason = resp.reason
+            real_url = resp.real_url
+        if status == 405:
+            # HEAD request not allowed, fall back on GET
+            resp = await session.get(url=url, allow_redirects=True, proxy=proxy, ssl=False, headers=HEADERS)
+            async with resp:
+                status = resp.status
+                reason = resp.reason
+                real_url = resp.real_url
+        return (status, reason, real_url)
+    except aiohttp.InvalidURL as e:
+        return (900, str(e), url)
+    except aiohttp.ClientConnectorError:
+        return (901, "Unreachable", url)
+    except gaierror as e:
+        return (902, str(e), url)
+    except aiohttp.ServerDisconnectedError as e:
+        return (903, str(e), url)
+    except aiohttp.ClientOSError as e:
+        return (904, str(e), url)
+    except TooManyRedirects as e:
+        return (905, str(e), url)
+    except aiohttp.ClientResponseError as e:
+        return (906, str(e), url)
+    except aiohttp.ServerTimeoutError:
+        return (907, "Connection timeout", url)
+    except asyncio.TimeoutError:
+        return (908, "Connection timeout", url)
--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-from typing import Union
 from fastapi import FastAPI
-import urllib.parse
-from .checker import check_socket, site_is_online_async
+from .checker import check_socket, add_scheme, get_status_code, HttpClient
 
 
 app = FastAPI()
+http_client = HttpClient()
 
 
 @app.get("/")
@@ -12,18 +11,33 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/check_url/{url}")  # url parameter should be quoted: urllib.parse.quote('http://www.google.com', safe='')
+@app.on_event("startup")
+async def startup():
+    http_client.start()
+
+
+@app.get("/check_url/")
 async def check_url(url: str):
-    url = urllib.parse.unquote(url).strip()
-    print(url)
+    urls = add_scheme(url=url)
     port_status = check_socket(host='127.0.0.1', port=9999)
     if port_status:
         proxy = 'http://127.0.0.1:9999'
     else:
         proxy = None
-    print(proxy)
-    try:
-        result = await site_is_online_async(url, proxy)
-    except Exception:
-        result = False
-    return {url: result}
+    results = {}
+    for i in urls:
+        result = await get_status_code(session=http_client.session, url=i, proxy=proxy)
+        results[i] = result
+    print(results)
+    # set http_scheme url as default derived_url
+    derived_url = urls[1]
+    url_status = 'unavailable'
+    for v in results.values():
+        if v[0] == 200:
+            derived_url = str(v[2])
+            url_status = 'available'
+    return {
+        'original_url': url,
+        'derived_url': derived_url,
+        'url_status': url_status
+    }
